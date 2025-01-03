c772b81fa3bf7012e820fc16b5189b510acdb07f
Bryan Suarez <bryansf2004@gmail.com>
2025-01-03 18:57:36 -0500

    template react-tailwind

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <div className='bg-blue-500 min-h-screen grid place-items-center'>
+        <div className='text-3xl font-bold'>
+          REACT
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
