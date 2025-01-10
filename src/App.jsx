8ef8711c71831c82240d9fcf7bf708234025d593
Bryan Suarez <bryansf2004@gmail.com>
2025-01-09 23:33:34 -0500

    v1.1.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
-
-import './App.css'
+import { Start } from "./components";
 
 function App() {
- 
-
   return (
     <>
-      <div className='bg-blue-500 min-h-screen grid place-items-center'>
-        <div className='text-3xl font-bold'>
-          REACT
+      <div className="bg-blue-500 min-h-screen grid place-items-center">
+        <div className="text-3xl font-bold">
+          <Start />
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Start.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const Start = () => {
+  return (
+    <div className='p-2 bg-blue-300 rounded-md'>Bienvenido a React!!!</div>
+  )
+}
+
+ 
+
+
+
+
+
+
+
